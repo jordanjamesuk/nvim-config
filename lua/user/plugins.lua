e44080a5633fca14b1ca12e22597625f1b337f60
Jordan James <jordan.james@waracle.com>
2023-05-19 11:49:30 +0100

    updates

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -107,6 +107,9 @@ return packer.startup(function(use)
   use { 'tmhedberg/SimpylFold' }
 
   use {'simrat39/rust-tools.nvim'}
+  
+  -- tagbar
+  use { 'preservim/tagbar' }
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
